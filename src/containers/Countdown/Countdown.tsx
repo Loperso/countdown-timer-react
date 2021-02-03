6759bcc4bcf334aabcbf88fd19e05528447bd1b5
Cristian Soler <loperso1010@gmail.com>
2021-02-03 11:07:38 -0500

    Se crean los contenedores de cronometro y temporizador y el componente de navegacion

---
+++ src/containers/Countdown/Countdown.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Countdown = () => {
+    return (
+        <div>
+            <p>CONTADOR</p>
+            <p>BOTONES (INICIAR, DETENER)</p>
+        </div>
+    );
+}
+
+export default Countdown;
---
+++ src/containers/Stopwatch/Stopwatch.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Stopwatch = () => {
+    return (
+        <div>
+            <p>CONTADOR (NO MODIFICABLE)</p>
+            <p>BOTONES (INICIAR, DIVIDIR, DETENER)</p>
+            <p>TABLA DE TIEMPO CUANDO SE DA AL BOTON DE DIVIDIR</p>
+        </div>
+    );
+}
+
+export default Stopwatch;
